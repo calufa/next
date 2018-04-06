a46995b5b8f5cb0ca0406aa3923c46911f25de8f
Carlos Chinchilla <calufa@gmail.com>
2018-04-05 18:15:31 -0700

    add type

--- crop-imgs/run.py
+++ crop-imgs/run.py
@@ -26,12 +26,12 @@ if __name__ == '__main__':
     parser.add_argument('--job-name')
     parser.add_argument('--imgs-path')
     parser.add_argument('--output-path', default=output_path)
-    parser.add_argument('--top', default=top)
-    parser.add_argument('--left', default=left)
-    parser.add_argument('--crop-width', default=crop_width)
-    parser.add_argument('--crop-height', default=crop_height)
-    parser.add_argument('--resize-width', default=resize_width)
-    parser.add_argument('--resize-height', default=resize_height)
+    parser.add_argument('--top', type=float, default=top)
+    parser.add_argument('--left', type=float, default=left)
+    parser.add_argument('--crop-width', type=int, default=crop_width)
+    parser.add_argument('--crop-height', type=int, default=crop_height)
+    parser.add_argument('--resize-width', type=int, default=resize_width)
+    parser.add_argument('--resize-height', type=int, default=resize_height)
     args = parser.parse_args()
 
     for k, v in args._get_kwargs():
@@ -40,8 +40,8 @@ if __name__ == '__main__':
     job_name = args.job_name
     imgs_path = args.imgs_path
     output_path = args.output_path
-    top = float(args.top)
-    left = float(args.left)
+    top = args.top
+    left = args.left
     crop_width = args.crop_width
     crop_height = args.crop_height
     resize_width = args.resize_width
