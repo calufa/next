29d5f70900996684bc2075fd27e4d6f1e317ff56
Carlos Chinchilla <calufa@gmail.com>
2018-05-07 22:30:09 -0700

    add sync code script

---
+++ sync.sh
@@ -0,0 +1,62 @@
+dir="combine-imgs"
+gcloud compute scp ./$dir gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+dir="crop-imgs"
+gcloud compute scp ./$dir gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+dir="face2landmarks"
+gcloud compute scp ./$dir gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+dir="pix2pix-infer"
+gcloud compute scp ./$dir gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+dir="pix2pix-trainer"
+gcloud compute scp ./$dir gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+dir="video"
+gcloud compute scp ./$dir gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+file="build.sh"
+gcloud compute scp ./$file gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+file="infer.sh"
+gcloud compute scp ./$file gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
+
+file="sync.sh"
+gcloud compute scp ./$file gpu:/home/next \
+  --compress \
+  --recurse \
+  --project magggenta-176803 \
+  --zone us-central1-c
